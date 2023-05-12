215d175829a3fd78accdd29da8edbf68b57b71b8
Amirlasalle <mirolasalle@gmail.com>
2023-05-12 17:27:30 -0500

    implemented child attributes.

---
+++ index.js
@@ -0,0 +1,29 @@
+// const inquirer = require("inquirer");
+// const fs = require("fs")
+
+// const questions = [
+//     {
+//         type:"list",
+//         message:"What type of shape do you want?",
+//         name:"shape"
+//     },
+//     {
+//         type:"input",
+//         message:"Enter Project Title",
+//         name:"title"
+//     }
+// ]
+var tvObj = 
+    {
+        make: "hisense",
+        model:"hisense led",
+        size: 65
+    }
+class tvObj {
+    constructor(make, model, size){
+        this.make = make,
+        this.model = model,
+        this.size = size
+    }
+}
+
--- library/svg.js
+++ library/svg.js
@@ -5,12 +5,20 @@ class SVG{
     }
     render(){
         return`
-        <svg width="100" height="100">
-        ${this.shapeEl}: ${this.textEl}
-  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+        <svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg">
+        ${this.shapeEl} ${this.textEl}
 </svg>
         `
 
+    }
+    setText(message, color){
+        if(message.length > 3){
+        console.error("ERROR ERRORER TEXT SHOULD BE LESS THAN 3");
+        } 
+        this.textEl = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}" </text>`
+    }
+    setShape(shape){
+        this.shapeEl = shape.render()
     }
 
 }
