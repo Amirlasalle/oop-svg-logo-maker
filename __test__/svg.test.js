f85e93831793bb6e479124528f3542233c393434
Amirlasalle <mirolasalle@gmail.com>
2023-06-03 15:33:58 -0500

    created a new logo for demo purposes.

--- __test__/svg.test.js
+++ __test__/svg.test.js
@@ -14,11 +14,10 @@ describe("Circle", function () {
         const returnShape = TestSVG.render()
         expect(returnShape).toEqual(expectedEl)
     })
-    test("ERROR ERRORER TEXT SHOULD BE LESS THAN 3", function () {
-        const expectedEl = new Error("ERROR ERRORER TEXT SHOULD BE LESS THAN 3")
-        const TestSVG = new SVG()
-        expect(() => TestSVG.setText("アミール", "white")).toThrow("ERROR ERRORER TEXT SHOULD BE LESS THAN 3")
-
+     test("ERROR ERRORER TEXT SHOULD BE LESS THAN 3", function () {
+        const expectedEl = new Error("Text must not exceed 3 characters.")
+        const hello = new SVG()
+        expect(() => hello.setText("HELLO", "#333")).toThrow(expectedEl)
     })
     test("Testing shape element", function () {
         const expectedEl = `<svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="red" /></svg>`
--- library/svg.js
+++ library/svg.js
@@ -8,11 +8,11 @@ class SVG {
     }
     setText(message, color) {
         if (message.length > 3) {
-            // console.error ("ERROR ERRORER TEXT SHOULD BE LESS THAN 3");
-            throw new Error("ERROR ERRORER TEXT SHOULD BE LESS THAN 3")
+        throw new Error("Text must not exceed 3 characters.");
         }
-        this.textEl =`<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`
-    }
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`;
+      }
+
     setShape(shape) {
         this.shapeEl = shape.render();
 
