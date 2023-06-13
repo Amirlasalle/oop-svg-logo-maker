da256d18eea7a4f8863a0065c65ababc9f8a5485
Amirlasalle <mirolasalle@gmail.com>
2023-06-12 21:23:36 -0500

    fixed up errors

--- __test__/svg.test.js
+++ __test__/svg.test.js
@@ -2,25 +2,25 @@ const SVG = require("../library/svg")
 const { Circle } = require("../library/shapes")
 describe("Circle", function () {
     test("rendering the svg element of size 300x300", function () {
-        const expectedEl = `<svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg"></svg>`
+        const expectedEl = `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg"></svg>`
         const TestSVG = new SVG()
         const returnShape = TestSVG.render()
         expect(returnShape).toEqual(expectedEl)
     })
     test("Testing rendering SVG element", function () {
-        const expectedEl = `<svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white"</text></svg>`
+        const expectedEl = `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">AJM</text></svg>`
         const TestSVG = new SVG()
-        TestSVG.setText("white")
+        TestSVG.setText("AJM", "white")
         const returnShape = TestSVG.render()
         expect(returnShape).toEqual(expectedEl)
     })
      test("ERROR ERRORER TEXT SHOULD BE LESS THAN 3", function () {
         const expectedEl = new Error("Text must not exceed 3 characters.")
         const hello = new SVG()
-        expect(() => hello.setText("HELLO", "#333")).toThrow(expectedEl)
+        expect(() => hello.setText("HELLO","#333")).toThrow(expectedEl)
     })
     test("Testing shape element", function () {
-        const expectedEl = `<svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="red" /></svg>`
+        const expectedEl = `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="red" /></svg>`
         const testCircle = new Circle()
         testCircle.setColor("red")
 
--- library/svg.js
+++ library/svg.js
@@ -4,13 +4,13 @@ class SVG {
         this.shapeEl = "";
     }
     render() {
-        return `<svg width="300" height="300" xmlns="http://wwww.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`
+        return `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`
     }
     setText(message, color) {
-        if (message.length > 3) {
+        if (message && message.length > 3) {
         throw new Error("Text must not exceed 3 characters.");
         }
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`;
+        this.textEl = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`;
       }
 
     setShape(shape) {
